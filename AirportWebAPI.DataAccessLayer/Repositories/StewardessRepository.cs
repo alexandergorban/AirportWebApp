71928a0225b086d67101c9a105fb15f6920fae96
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-13 17:39:05 +0300

    Updated StewardessRepository

--- AirportWebAPI.DataAccessLayer/Repositories/StewardessRepository.cs
+++ AirportWebAPI.DataAccessLayer/Repositories/StewardessRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using AirportWebAPI.DataAccessLayer.Interfaces;
 using AirportWebAPI.DataAccessLayer.Models;
@@ -8,19 +9,30 @@ namespace AirportWebAPI.DataAccessLayer.Repositories
 {
     class StewardessRepository : IRepository<Stewardess>
     {
+        private IAirportContext _context;
+
+        public StewardessRepository(IAirportContext context)
+        {
+            _context = context;
+        }
+
         public IEnumerable<Stewardess> GetEntities(IEnumerable<Guid> entityIds)
         {
-            throw new NotImplementedException();
+            return _context.Stewardesses.Where(s => entityIds.Contains(s.Id))
+                .OrderBy(s => s.DateOfBirth)
+                .OrderBy(s => s.Surname)
+                .ToList();
         }
 
         public Stewardess GetEntity(Guid entityId)
         {
-            throw new NotImplementedException();
+            return _context.Stewardesses.FirstOrDefault(s => s.Id == entityId);
         }
 
         public void AddEntity(Stewardess entity)
         {
-            throw new NotImplementedException();
+            entity.Id = Guid.NewGuid();
+            _context.Stewardesses.Add(entity);
         }
 
         public void UpdateEntity(Stewardess entity)
@@ -30,17 +42,17 @@ namespace AirportWebAPI.DataAccessLayer.Repositories
 
         public void DeleteEntity(Stewardess entity)
         {
-            throw new NotImplementedException();
+            _context.Stewardesses.Remove(entity);
         }
 
         public bool EntityExists(Guid entityId)
         {
-            throw new NotImplementedException();
+            return _context.Stewardesses.Any(s => s.Id == entityId);
         }
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            return (_context.SaveChanges() >= 0);
         }
     }
 }
