b967878117048f536dc69859a671fc848a4ee3a3
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-13 21:04:14 +0300

    Updated IRepository<TEntity>

--- AirportWebAPI.DataAccessLayer/Interfaces/IRepository.cs
+++ AirportWebAPI.DataAccessLayer/Interfaces/IRepository.cs
@@ -4,13 +4,13 @@ using System.Text;
 
 namespace AirportWebAPI.DataAccessLayer.Interfaces
 {
-    public interface IRepository<T> where T : IEntity
+    public interface IRepository<TEntity> where TEntity : IEntity
     {
-        IEnumerable<T> GetEntities(IEnumerable<Guid> entityIds);
-        T GetEntity(Guid entityId);
-        void AddEntity(T entity);
-        void UpdateEntity(T entity);
-        void DeleteEntity(T entity);
+        IEnumerable<TEntity> GetEntities(IEnumerable<Guid> entityIds);
+        TEntity GetEntity(Guid entityId);
+        void AddEntity(TEntity entity);
+        void UpdateEntity(TEntity entity);
+        void DeleteEntity(TEntity entity);
         bool EntityExists(Guid entityId);
         bool Save();
     }
