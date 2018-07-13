af5b10e5a5277fa6edcd8428263fcc1bc5f412db
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-13 22:54:15 +0300

    Added IService<TEntity>

---
+++ AirportWebAPI.BusinessLayer/Interfaces/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AirportWebAPI.DataAccessLayer.Interfaces;
+using Shared.Interfaces;
+
+namespace AirportWebAPI.BusinessLayer.Interfaces
+{
+    interface IService<TEntity> where TEntity : IModelDto
+    {
+        List<TEntity> GetEntities();
+        TEntity GetEntity(TEntity entity);
+        TEntity AddEntity(TEntity entity);
+        TEntity UpdateEntity(TEntity entity);
+        void DeleteEntity(TEntity entity);
+    }
+}
--- Shared/Interfaces/IModelDto.cs
+++ Shared/Interfaces/IModelDto.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Shared.Interfaces
 {
-    interface IModelDto
+    public interface IModelDto
     {
     }
 }
