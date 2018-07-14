36d3f7ceb709d5fce47362487df074c70a2cdde0
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-14 22:59:07 +0300

    Added basic AirportDbContext

---
+++ AirportWebAPI.DataAccessLayer/Data/AirportDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AirportWebAPI.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirportWebAPI.DataAccessLayer.Data
+{
+    class AirportDbContext : DbContext
+    {
+        public DbSet<Airplane> Airplanes { get; set; }
+        public DbSet<AirplaneType> AirplaneTypes { get; set; }
+        public DbSet<AirportLocation> AirportLocations { get; set; }
+        public DbSet<Crew> Crews { get; set; }
+        public DbSet<Departure> Departures { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Pilot> Pilots { get; set; }
+        public DbSet<Stewardess> Stewardesses { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
