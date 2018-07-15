612cdd0bd738cd44d62509a9f3b91080cc6a830a
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-15 16:01:33 +0300

    Added first migration

---
+++ AirportWebAPI.DataAccessLayer/Migrations/20180715125941_AirportDb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirportWebAPI.DataAccessLayer.Migrations
+{
+    public partial class AirportDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AirportLocations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    AirportName = table.Column<string>(maxLength: 50, nullable: false),
+                    Country = table.Column<string>(maxLength: 30, nullable: false),
+                    City = table.Column<string>(maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AirportLocations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AirportLocations");
+        }
+    }
+}
