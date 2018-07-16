0ee04f7682302fe85cfbe2be8ff2a01439b8369a
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-16 16:15:15 +0300

    Added abstract class BaseRepository

---
+++ AirportWebAPI.DataAccessLayer/Abstractions/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AirportWebAPI.DataAccessLayer.Interfaces;
+
+namespace AirportWebAPI.DataAccessLayer.Abstractions
+{
+    abstract class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : IEntity
+    {
+        public IEnumerable<TEntity> GetEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetEntity(Guid entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddEntity(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEntity(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEntity(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EntityExists(Guid entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
