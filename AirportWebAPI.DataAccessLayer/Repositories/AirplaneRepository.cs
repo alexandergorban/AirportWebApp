f41ecb723023aa28d95d266fd3de773fc2c94557
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-16 17:55:39 +0300

    Updated GetEntities() for Airplane

--- AirportWebAPI.DataAccessLayer/Repositories/AirplaneRepository.cs
+++ AirportWebAPI.DataAccessLayer/Repositories/AirplaneRepository.cs
@@ -7,14 +7,24 @@ using AirportWebAPI.DataAccessLayer.Data;
 using AirportWebAPI.DataAccessLayer.Interfaces;
 using AirportWebAPI.DataAccessLayer.Entities;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 
 namespace AirportWebAPI.DataAccessLayer.Repositories
 {
     public class AirplaneRepository : BaseRepository<Airplane>
     {
+        private readonly AirportDbContext _context;
+
         public AirplaneRepository(AirportDbContext context, IMapper mapper)
             : base(context, mapper)
         {
+            _context = context;
+        }
+
+        public override IEnumerable<Airplane> GetEntities()
+        {
+            return _context.Airplanes
+                .Include(a => a.AirplaneType);
         }
     }
 }
