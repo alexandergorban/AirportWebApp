fdd9ef96c9a4e59deb61785c356f835b4152077e
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-07-17 22:08:02 +0300

    Added NUnit package

--- AirportWebApi.Tests/UnitTest1.cs
+++ AirportWebApi.Tests/UnitTest1.cs
@@ -1,12 +1,10 @@
 using System;
-using Xunit;
 
 namespace AirportWebApi.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+Û        public void Test1()
         {
 
         }
